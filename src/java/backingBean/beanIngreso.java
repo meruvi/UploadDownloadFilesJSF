4fb5b27968ffd13017613fe34c5d125811652093
meruvi <gmeruviarojas@gmail.com>
2019-10-12 08:11:02 -0400

    Creacion de BEAN

---
+++ src/java/backingBean/beanIngreso.java
@@ -0,0 +1,41 @@
+package backingBean;
+
+import controller.ControllerLogIn;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class beanIngreso {
+
+    private String user;
+    private String pass;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    public beanIngreso() {
+    }
+    
+    public String ingresoAlSistema(){
+        ControllerLogIn controllerLogIn = new ControllerLogIn();
+        if(controllerLogIn.LogIn(user, pass)){
+            return "yes";
+        }else{
+            return "no";
+        }
+    }
+}
